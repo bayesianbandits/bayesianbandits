f9235dffb66960222460183fcf7e1677ca7b54f1
Rishi Kulkarni <rishi@kulkarni.science>
2023-03-01 20:31:25 -0500

    TEST: use tempfile for persistence tests

--- tests/test_bandit_pickling.py
+++ tests/test_bandit_pickling.py
@@ -1,8 +1,17 @@
+import tempfile
+
 import joblib
 from numpy.testing import assert_almost_equal
+import pytest
 
 
 from bayesianbandits import Bandit, Arm, epsilon_greedy, GammaRegressor
+
+
+@pytest.fixture(scope="module")
+def temp_model_file():
+    with tempfile.NamedTemporaryFile() as f:
+        yield f
 
 
 def action1():
@@ -27,35 +36,37 @@ class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
     arm3 = Arm(action3, reward_func)
 
 
-def test_pickle_and_load():
+def test_pickle_and_load(temp_model_file):
     agent = Agent(rng=1)
 
     agent.pull()
     agent.update(1)
     assert_almost_equal(agent.arm1.learner.coef_[1], [2, 2])  # type: ignore
 
-    joblib.dump(agent, "agent.pkl")
+    joblib.dump(agent, temp_model_file)
+    temp_model_file.seek(0)
 
-    loaded = joblib.load("agent.pkl")
+    loaded = joblib.load(temp_model_file)
 
     assert_almost_equal(loaded.arm1.learner.coef_[1], agent.arm1.learner.coef_[1])
 
 
-def test_remove_arm():
+def test_remove_arm(temp_model_file):
     global Agent
 
     class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
         arm1 = Arm(action1, reward_func)
         arm2 = Arm(action2, reward_func)
 
-    loaded = joblib.load("agent.pkl")
+    temp_model_file.seek(0)
+    loaded = joblib.load(temp_model_file)
 
     assert_almost_equal(loaded.arm1.learner.coef_[1], [2, 2])  # type: ignore
 
     assert not hasattr(loaded, "arm3")
 
 
-def test_new_arm():
+def test_new_arm(temp_model_file):
     global Agent
 
     class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
@@ -63,7 +74,8 @@ def test_new_arm():
         arm2 = Arm(action2, reward_func)
         arm4 = Arm(action3, reward_func)
 
-    loaded = joblib.load("agent.pkl")
+    temp_model_file.seek(0)
+    loaded = joblib.load(temp_model_file)
 
     assert_almost_equal(loaded.arm1.learner.coef_[1], [2, 2])  # type: ignore
 
