386efeaac124c9d420c672a210b0cd661deec582
Rishi Kulkarni <rishi@kulkarni.science>
2023-07-05 20:06:59 -0400

    TEST: add tests for delayed reward update warnings

--- tests/test_bandit_pickling.py
+++ tests/test_bandit_pickling.py
@@ -3,9 +3,18 @@ import tempfile
 import joblib
 from numpy.testing import assert_almost_equal
 import pytest
+from typing import IO
+import numpy as np
+from numpy.typing import NDArray
 
 
-from bayesianbandits import Bandit, Arm, epsilon_greedy, GammaRegressor
+from bayesianbandits import (
+    Bandit,
+    Arm,
+    epsilon_greedy,
+    GammaRegressor,
+    DelayedRewardWarning,
+)
 
 
 @pytest.fixture(scope="module")
@@ -26,7 +35,7 @@ def action3():
     print("action3")
 
 
-def reward_func(x):
+def reward_func(x: NDArray[np.float_]) -> NDArray[np.float_]:
     return x
 
 
@@ -36,7 +45,7 @@ class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
     arm3 = Arm(action3, reward_func)
 
 
-def test_pickle_and_load(temp_model_file):
+def test_pickle_and_load(temp_model_file: IO[bytes]):
     agent = Agent(rng=1)
 
     agent.pull()
@@ -51,7 +60,7 @@ def test_pickle_and_load(temp_model_file):
     assert_almost_equal(loaded.arm1.learner.coef_[1], agent.arm1.learner.coef_[1])
 
 
-def test_remove_arm(temp_model_file):
+def test_remove_arm(temp_model_file: IO[bytes]):
     global Agent
 
     class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
@@ -66,7 +75,7 @@ def test_remove_arm(temp_model_file):
     assert not hasattr(loaded, "arm3")
 
 
-def test_new_arm(temp_model_file):
+def test_new_arm(temp_model_file: IO[bytes]):
     global Agent
 
     class Agent(Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy()):
@@ -83,3 +92,60 @@ def test_new_arm(temp_model_file):
 
     assert hasattr(loaded, "arm4")
     assert loaded.arm4.learner.random_state == loaded.arm1.learner.random_state
+
+
+class DelayedRewardAgent(
+    Bandit, learner=GammaRegressor(1, 1), policy=epsilon_greedy(), delayed_reward=True
+):
+    arm1 = Arm(action1, reward_func)
+    arm2 = Arm(action2, reward_func)
+    arm3 = Arm(action3, reward_func)
+
+
+def test_removed_arm_update_warning(temp_model_file: IO[bytes]):
+    global DelayedRewardAgent
+
+    agent = DelayedRewardAgent(rng=1)
+    agent.pull(unique_id=1)
+    temp_model_file.seek(0)
+    joblib.dump(agent, temp_model_file)
+
+    class DelayedRewardAgent(
+        Bandit,
+        learner=GammaRegressor(1, 1),
+        policy=epsilon_greedy(),
+        delayed_reward=True,
+    ):
+        arm2 = Arm(action1, reward_func)
+        arm3 = Arm(action2, reward_func)
+
+    temp_model_file.seek(0)
+    loaded = joblib.load(temp_model_file)
+
+    with pytest.warns(DelayedRewardWarning):
+        loaded.update(1, unique_id=1)
+
+
+def test_removed_arm_update_warning_batch():
+    global DelayedRewardAgent
+
+    with tempfile.NamedTemporaryFile() as f:
+        agent = DelayedRewardAgent(rng=1)
+        agent.pull(unique_id=1)
+        agent.pull(unique_id=2)
+        joblib.dump(agent, f)
+
+        class DelayedRewardAgent(
+            Bandit,
+            learner=GammaRegressor(1, 1),
+            policy=epsilon_greedy(),
+            delayed_reward=True,
+        ):
+            arm1 = Arm(action1, reward_func)
+            arm3 = Arm(action2, reward_func)
+
+        f.seek(0)
+        loaded = joblib.load(f)
+
+    with pytest.warns(DelayedRewardWarning):
+        loaded.update([1, 2], unique_id=[1, 2])
