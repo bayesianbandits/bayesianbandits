48966069116e17be1117d3be192fe77d961e6138
Rishi Kulkarni <rishi@kulkarni.science>
2024-12-09 22:20:12 -0500

    REFAC: Improve type hinting for test functions in solver detection tests

--- tests/test_solver_detection.py
+++ tests/test_solver_detection.py
@@ -4,16 +4,16 @@ import pytest
 
 
 @pytest.fixture
-def no_cholmod(monkeypatch):
+def no_cholmod(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setitem(sys.modules, "sksparse.cholmod", None)
 
 
 @pytest.fixture
-def no_suitesparse_env_vars(monkeypatch):
+def no_suitesparse_env_vars(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setenv("BB_NO_SUITESPARSE", "1")
 
 
-def test_no_suitesparse(no_cholmod):
+def test_no_suitesparse(no_cholmod: None):
     if "bayesianbandits._sparse_bayesian_linear_regression" in sys.modules:
         del sys.modules["bayesianbandits._sparse_bayesian_linear_regression"]
     from bayesianbandits._sparse_bayesian_linear_regression import SparseSolver, solver
@@ -21,7 +21,7 @@ def test_no_suitesparse(no_cholmod):
     assert solver == SparseSolver.SUPERLU
 
 
-def test_yes_cholmod_no_umfpack():
+def test_yes_cholmod_no_umfpack() -> None:
     if "bayesianbandits._sparse_bayesian_linear_regression" in sys.modules:
         del sys.modules["bayesianbandits._sparse_bayesian_linear_regression"]
     from bayesianbandits._sparse_bayesian_linear_regression import SparseSolver, solver
@@ -29,7 +29,7 @@ def test_yes_cholmod_no_umfpack():
     assert solver == SparseSolver.CHOLMOD
 
 
-def test_yes_cholmod_yes_umfpack():
+def test_yes_cholmod_yes_umfpack() -> None:
     if "bayesianbandits._sparse_bayesian_linear_regression" in sys.modules:
         del sys.modules["bayesianbandits._sparse_bayesian_linear_regression"]
     from bayesianbandits._sparse_bayesian_linear_regression import SparseSolver, solver
@@ -37,7 +37,7 @@ def test_yes_cholmod_yes_umfpack():
     assert solver == SparseSolver.CHOLMOD
 
 
-def test_no_suitesparse_env_vars(no_suitesparse_env_vars):
+def test_no_suitesparse_env_vars(no_suitesparse_env_vars: None):
     if "bayesianbandits._sparse_bayesian_linear_regression" in sys.modules:
         del sys.modules["bayesianbandits._sparse_bayesian_linear_regression"]
     from bayesianbandits._sparse_bayesian_linear_regression import SparseSolver, solver
